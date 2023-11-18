3c1b77706b3513de59959ece529c2f0afa24819c
Aiman <aimanizales@gmail.com>
2023-11-18 00:27:13 -0500

    WIP: fix issue Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
-import './globals.css'
+import React from 'react';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,4 +15,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
         <body className={inter.className}>{children}</body>
     </html>
-)};
+)}
